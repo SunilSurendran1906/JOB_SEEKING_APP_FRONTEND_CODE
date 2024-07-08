5ed34d33bd694eb06eb83abfda25b51e9afa25e1
SUNIL SURENDRAN <133184647+SunilSurendran1906@users.noreply.github.com>
2024-07-08 14:49:24 +0530

    JOB-SEEKING-APP-FRONTEND

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { useContext, useEffect } from "react";
+import { Context } from "../../main";
+import { Navigate } from "react-router-dom";
+import HeroSection from "./HeroSection";
+import HowItWorks from "./HowItWorks";
+import PopularCategories from "./PopularCategories";
+import PopularCompanies from "./PopularCompanies";
+import axios from "axios";
+
+const Home = () => {
+  const { isAuthorized, setIsAuthorized, setUser } = useContext(Context);
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:4000/api/v1/user/getuser",
+          {
+            withCredentials: true,
+          }
+        );
+        setUser(response.data.user);
+        setIsAuthorized(true);
+      } catch (error) {
+        setIsAuthorized(false);
+      }
+    };
+    fetchUser();
+  }, [isAuthorized]);
+  if (!isAuthorized) {
+    return <Navigate to={"/login"} />;
+  }
+  return (
+    <>
+      <section className="homePage page">
+        <HeroSection />
+        <HowItWorks />
+        <PopularCategories />
+        <PopularCompanies />
+      </section>
+    </>
+  );
+};
+
+export default Home;
